566b6ac44492bcd869cf7e6354294b6fa3ee5289
KYapp <kukiyomenaisapp@gmail.com>
2023-03-19 19:17:53 +0900

    hoge

--- NgrokSpeedTestClient/Program.cs
+++ NgrokSpeedTestClient/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace NgrokSpeedTestClient
+﻿using WebSocketSharp;
+
+WebSocket webSocket = new WebSocket("wss://24f0-2404-7a81-80c0-d200-d199-239a-aba-b917.jp.ngrok.io/");
+
+webSocket.OnClose += (sender, e) => Console.WriteLine("close");
+webSocket.OnError += (sender, e) => Console.WriteLine("error");
+webSocket.OnMessage += (sender, e) =>
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+    Console.WriteLine("message");
+    webSocket.Send("hoge");
+};
+webSocket.OnOpen += (sender, e) =>
+{
+    Console.WriteLine("open");
+    webSocket.Send("hoge");
+};
+
+webSocket.Connect();
+webSocket.Send("hoge");
+
+while (true) ;
